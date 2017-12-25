b1e61cc7075c9d8c6c93f8d0747d5267e261f3fe
Katskel <katskel1999@tut.by>
2017-12-25 19:09:16 +0300

    MyAwesomeCommit

---
+++ EnrollmentWebApp/EnrollmentWebApp.Domain/Abstract/IDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using EnrollmentWebApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnrollmentWebApp.Domain.Abstract
+{
+    public interface IDataRepository
+    {
+        IEnumerable<Enrollee> Enrollees { get; }
+
+        void SaveEnrollee(Enrollee enrollee);
+        Enrollee DeleteEnrollee(int enrolleeID);
+    }
+}
---
+++ EnrollmentWebApp/EnrollmentWebApp.Domain/Entities/Enrollee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentWebApp.Domain.Entities
+{
+    public class Enrollee
+    {
+        public int EnrolleeId { get; set; }
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your surname")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Please enter your patronymic name")]
+        public string Patronymic { get; set; }
+        [Required(ErrorMessage = "Please enter your passport number")]
+        public string Passport { get; set; }
+        [Required(ErrorMessage = "Please enter your birthdate")]
+        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "Please enter your town")]
+        public string Town { get; set; }
+        [Required(ErrorMessage = "Please enter your address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string PhoneNumber { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter number in 0..100 interval")]
+        public int CTLanguage { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter number in 0..100 interval")]
+        public int CTFirstSubject { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter number in 0..100 interval")]
+        public int CTSecondSubject { get; set; }
+
+        [Required(ErrorMessage = "Please enter your average score in school")]
+        [Range(0, 10.0, ErrorMessage = "Please enter average score in 0..10 interval")]
+        public decimal AverageScore { get; set; }
+    }
+}
